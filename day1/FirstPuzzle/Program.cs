4f938c8a1dd49dc7e32c641af3e8c49d7a6d37d8
Tomasz Zając <tomekdiego1998@gmail.com>
2019-12-01 12:28:44 +0100

    First Puzzle

---
+++ day1/FirstPuzzle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstPuzzle
+{
+    static class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine(await LoadData()
+                .GetIntValues()
+                .CalculateFuelNeededForModules()
+                .ReduceResult());
+        }
+
+        private async static Task<string[]> LoadData() => await File.ReadAllLinesAsync("input.txt");
+        private async static Task<IEnumerable<int>> GetIntValues(this Task<string[]> input) => (await input).Select(i => int.Parse(i));
+        private async static Task<IEnumerable<int>> CalculateFuelNeededForModules(this Task<IEnumerable<int>> input) =>
+            (await input).Select(i => (i / 3) - 2);
+        private async static Task<int> ReduceResult(this Task<IEnumerable<int>> input) => (await input).Sum();
+    }
+    
+}
