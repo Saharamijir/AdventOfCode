17ab2766c3543aae7bf030104dfdf45d4a3fe2fe
Tomasz Zając <tomekdiego1998@gmail.com>
2019-12-01 19:07:21 +0100

    Add f# sollution

---
+++ day1/fs/FirstPuzzle/Program.fs
@@ -0,0 +1,22 @@
+﻿open System.IO
+
+let LoadData =
+    File.ReadAllLines "input.txt"
+
+let GetIntValues input =
+    [for inputValue in input -> inputValue |> int]
+
+let CalculateFuel input =
+    [for inputValue in input -> inputValue/3 - 2]
+
+let ReduceResult input =
+    List.sum input
+
+[<EntryPoint>]
+let main argv =
+    LoadData
+        |>GetIntValues
+        |>CalculateFuel
+        |>ReduceResult
+        |>printf "%i"
+    0
---
+++ day1/fs/SecondPuzzle/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let LoadData =
+    [for value in File.ReadAllLines "input.txt" -> value |> int]
+
+let rec CalculateFuelForModule input =
+    let intermediate = input/3 - 2
+    if intermediate <= 0 then 0
+    else intermediate + CalculateFuelForModule intermediate
+
+let CalculateFuelForAllModules input =
+    [for inputValue in input -> inputValue |> CalculateFuelForModule]
+
+let ReduceResult input =
+    List.sum input
+
+[<EntryPoint>]
+let main argv =
+    LoadData
+        |>CalculateFuelForAllModules
+        |>ReduceResult
+        |>printf "%i"
+    0
