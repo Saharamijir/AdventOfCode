36555c4f41a23423f058f072aadc782928c04de0
Tomasz Zając <tomekdiego1998@gmail.com>
2019-12-05 23:04:03 +0100

    day 3 second part

---
+++ day3/fs/Program.fs
@@ -0,0 +1,45 @@
+﻿open System.IO
+open System.Collections.Generic
+let LoadData =
+  File.ReadAllLines "input.txt"
+  |> Array.map (fun l -> l.Split ',' |> Array.map (fun i -> (i.[0], (i.[1..] |> int))))
+
+
+let MakePoint previous command : int * int =
+  match command with
+  | 'U' | 'u' -> (fst previous, snd previous + 1)
+  | 'R' | 'r' -> (fst previous + 1, snd previous)
+  | 'D' | 'd' -> (fst previous, snd previous - 1)
+  | 'L' | 'l' -> (fst previous - 1, snd previous)
+  | _ -> failwith "wrong argument"
+
+let TransformPoints (command: char, times: int) (previous : List<int*int>)=
+  for _ in 1..times do
+    previous.Add (MakePoint (previous.Item (previous.Count - 1)) command)
+
+let FoldHelper (acc: List<int*int>) (elem : char * int)=
+  TransformPoints elem acc
+  acc
+
+let GetPoints arr=
+  let res = new List<int*int>()
+  res.Add (0, 0)
+  arr
+    |> Array.fold FoldHelper res
+
+[<EntryPoint>]
+let main _ =
+  let data = LoadData
+  let firstWire = GetPoints data.[0]
+  let secondWire = GetPoints data.[1]
+  let intersections =
+    Set.intersect
+        (firstWire |> Set.ofSeq)
+        (secondWire |> Set.ofSeq)
+    |> Set.filter (fun x -> x <> (0,0))
+  let mutable sequence = Seq.empty
+  for x in intersections do
+    sequence <- Seq.append sequence [(firstWire.IndexOf x) + (secondWire.IndexOf x)]
+  sequence |> Seq.min |> printfn "%i"
+
+  0 // return an integer exit code
